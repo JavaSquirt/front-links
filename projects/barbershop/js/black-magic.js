1d1b849fb8892c0c075ce876870766a8eaf15bac
vald3r <c58.krypt0n@gmail.com>
2017-06-05 00:26:33 +0300

    Added JabbaSquirt and yandex-map

--- projects/barbershop/js/black-magic.js
+++ projects/barbershop/js/black-magic.js
@@ -1,3 +1,5 @@
+/* --- MODAL --- */
+
 var link = document.querySelector(".login");
 var popup = document.querySelector(".modal-content");
 var close = popup.querySelector(".modal-content-close");
@@ -37,6 +39,32 @@ window.addEventListener("keydown", function(event) {
     if (popup.classList.contains("modal-content-show")) {
       popup.classList.remove("modal-content-show");
       popup.classList.remove("modal-error");
+    }
+  }
+});
+
+
+
+/* --- MAP --- */
+
+var mapOpen = document.querySelector(".js-open-map");
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = mapPopup.querySelector(".modal-content-close");
+
+mapOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.add("modal-content-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-content-show")) {
+      mapPopup.classList.remove("modal-content-show");
     }
   }
 });
